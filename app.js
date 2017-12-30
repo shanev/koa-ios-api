c06ab6d8106e7b644fb65f40e2bbdf6796446387
Shane Vitarana <shanev@gmail.com>
2017-12-31 00:18:36 +0530

    Changed device to deviceToken

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(async (ctx, next) => {
   const start = new Date();
   await next();
   const ms = new Date() - start;
-  debug(`${ctx.method} ${ctx.url} - ${ms}ms`);
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
 });
 
 // API
