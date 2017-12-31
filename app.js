adca81eba98ce23321d2c41306f8f84f19ebdf8b
Shane Vitarana <shanev@gmail.com>
2017-12-31 12:15:25 +0530

    Fixed exiting mocha properly

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ const app = new Koa();
 app.use(bodyParser());
 
 // middleware to output HTTP method and time
-app.use(async (ctx, next) => {
-  const start = new Date();
-  await next();
-  const ms = new Date() - start;
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
-});
+// app.use(async (ctx, next) => {
+//   const start = new Date();
+//   await next();
+//   const ms = new Date() - start;
+//   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+// });
 
 // API
 app.use(api.routes());
