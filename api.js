67020e0887d7ea5c8a88731e04697fdca3b34b62
Shane Vitarana <shanev@gmail.com>
2017-12-31 19:10:08 +0530

    Test updating device token

--- api.js
+++ api.js
@@ -41,27 +41,12 @@ router.get('/users/self', auth, async (ctx) => {
  */
 router.put('/users/self', auth, async (ctx) => {
   const userId = ctx.state.user.id;
-  const { body } = ctx.request.body;
+  const { body } = ctx.request;
   const keys = Object.keys(body);
   if (keys.length === 0) {
     throw new InvalidRequestError();
   }
   await User.findOneAndUpdate({ _id: userId }, body);
-  ctx.status = 200;
-});
-
-/**
- * POST /users/1/devices
- * Body Parameters: device
- * Save a user's device token.
- */
-router.post('/users/self/devices', auth, async (ctx) => {
-  const userId = ctx.state.user.id;
-  const device = ctx.request.body.device;
-  if (device == null) {
-    throw new InvalidRequestError();
-  }
-  await User.findOneAndUpdate({ _id: userId }, { device });
   ctx.status = 200;
 });
 
--- test/api-test.js
+++ test/api-test.js
@@ -6,7 +6,7 @@ const User = require('../app/user');
 describe('API', () => {
   describe('/api/v1/users', () => {
     describe('POST', () => {
-      it('should create a user', (done) => {
+      it('should create a new user', (done) => {
         request
           .post('/api/v1/users')
           .expect('Content-Length', '148')
@@ -28,20 +28,20 @@ describe('API', () => {
           });
       });
     });
+
+    describe('PUT', () => {
+      it('should update device token', (done) => {
+        User.find({})
+          .then((users) => {
+            const token = jwt.sign({ id: users[0].id }, process.env.JWT_SECRET);
+            const data = { device: { token: 'ad9s8f7a9ds8f7ads98f7' } };
+            request
+              .put('/api/v1/users/self')
+              .set('Authorization', `Bearer ${token}`)
+              .send(data)
+              .expect(200, done);
+          });
+      });
+    });
   });
-
-  // describe('/api/v1/users/self/devices', () => {
-  //   const token = jwt.sign({ id: '58de92c8d9ef7f4d0e11b9bc' }, process.env.JWT_SECRET);
-
-  //   describe('POST', () => {
-  //     it('should save device token', (done) => {
-  //       const data = { device: { token: 'ad9s8f7a9ds8f7ads98f7' } };
-  //       request
-  //         .post('/api/v1/users/self/devices')
-  //         .set('Authorization', `Bearer ${token}`)
-  //         .send(data)
-  //         .expect(200, done);
-  //     });
-  // });
-  // });
 });
